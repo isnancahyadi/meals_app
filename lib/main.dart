c72e4b9d5ecd1e6e7c418235d1d29e0b9255b4ed
isnancahyadi <isnan.arifc@gmail.com>
2023-02-27 16:19:55 +0700

    passing data with routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'category_meals_screen.dart';
 import 'categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               )),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
