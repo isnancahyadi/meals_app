968501b1fafe6daeb36fcc31dab300004743a62e
isnancahyadi <isnan.arifc@gmail.com>
2023-03-04 21:43:51 +0700

    fix bug

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -41,12 +41,13 @@ class MainDrawer extends StatelessWidget {
           _buildListTile(
             'Meals',
             Icons.restaurant,
-            () => Navigator.of(context).pushNamed('/'),
+            () => Navigator.of(context).pushReplacementNamed('/'),
           ),
           _buildListTile(
             'Filters',
             Icons.settings,
-            () => Navigator.of(context).pushNamed(FiltersScreen.routeName),
+            () => Navigator.of(context)
+                .pushReplacementNamed(FiltersScreen.routeName),
           ),
         ],
       ),
