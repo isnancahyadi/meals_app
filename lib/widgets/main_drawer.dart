a9615db945e8a3930871bbcb71524539f1948108
isnancahyadi <isnan.arifc@gmail.com>
2023-03-04 21:33:23 +0700

    add link to drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import '/screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget _buildListTile(String title, IconData icon) {
+  Widget _buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
       leading: Icon(icon, size: 26),
       title: Text(
@@ -12,9 +13,7 @@ class MainDrawer extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () {
-        // ...
-      },
+      onTap: tapHandler,
     );
   }
 
@@ -39,8 +38,16 @@ class MainDrawer extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
-          _buildListTile('Meals', Icons.restaurant),
-          _buildListTile('Filters', Icons.settings),
+          _buildListTile(
+            'Meals',
+            Icons.restaurant,
+            () => Navigator.of(context).pushNamed('/'),
+          ),
+          _buildListTile(
+            'Filters',
+            Icons.settings,
+            () => Navigator.of(context).pushNamed(FiltersScreen.routeName),
+          ),
         ],
       ),
     );
