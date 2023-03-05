ad34b928b161b18966e71b53d8a68e588404ad00
isnancahyadi <isnan.arifc@gmail.com>
2023-03-05 21:33:41 +0700

    adding temporary delete recipe

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -7,6 +7,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem({
     @required this.id,
@@ -15,6 +16,7 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
     @required this.complexity,
     @required this.duration,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -50,10 +52,16 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
+    Navigator.of(context)
+        .pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
-    );
+    )
+        .then((result) {
+      if (result != null) {
+        removeItem(result);
+      }
+    });
   }
 
   @override
